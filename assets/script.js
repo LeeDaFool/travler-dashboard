5e5909d855ca6d55d2154b2b2030ecea665fc39d
Chang Metellus <cmetellus@escaladesports.com>
2023-01-04 12:55:49 -0500

    working on getting input city

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Date
-var currentDate = moment().format("YYYY-MM-DD 00:00:00");
+var currentDate = moment().format("YYYY-MM-DD");
 // 5-day forecast
 var fiveDays = [
   moment().add(1, 'days').format("YYYY-MM-DD 00:00:00"),
@@ -8,7 +8,9 @@ var fiveDays = [
   moment().add(4, 'days').format("YYYY-MM-DD 00:00:00"),
   moment().add(5, 'days').format("YYYY-MM-DD 00:00:00")
 ];
-console.log(fiveDays);
+var forecastContainer = $("#forecastList");
+// console.log(forecastConainer.text);
+// console.log(fiveDays);
 
 // Current selected info
 var currentCity = document.querySelector(".current-city");
@@ -18,6 +20,7 @@ var currentTemp = document.querySelector(".current-temp");
 var currentWind = document.querySelector(".current-wind");
 var currentHumidity = document.querySelector(".current-humidity");
 
+
 var cityInput = document.querySelector(".city-input");
 // Buttons
 var submitBtn = document.querySelector(".save-current");
@@ -26,10 +29,14 @@ var recentCityBtn = document.querySelectorAll(".recent-city");
 var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
 
+  submitBtn.addEventListener("click", () => {
+    // let citySelection = cityInput.value;
+    console.log(cityInput.value);
+  });
+
+
 // Current city forecast
 function todaysWeather () {
-
-
 
   fetch("https://api.openweathermap.org/data/2.5/weather?units=imperial&q=orlando&appid=25c8bef2657790fdeaf81f2a54759430"
   
@@ -38,7 +45,7 @@ function todaysWeather () {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    // console.log(data);
     currentCity.textContent = data.name;
     currentCityDate.textContent = `${currentDate} `;
     currentTemp.textContent += ` ${data.main.temp}F`
@@ -58,15 +65,16 @@ function fiveDayForecast () {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    // console.log(data);
     for (let i = 0; i < data.list.length; i++) {
-      console.log(data.list[i].dt_txt);
+      // console.log(data.list[i].dt_txt);
+      for(let n = 0; n < fiveDays.length; n++) {
+        if(data.list[i].dt_txt == fiveDays[n])
+        console.log(data.list[i]);
+        let forecastData = data.list[i];
 
-    }
-    
-
-    
-    
+      }
+    }  
   });
 }
 
